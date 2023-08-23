2e864e739eaf5334c0310957692a1d7de49b9cd6
Sebastian <thiagohugo2014@gmail.com>
2023-08-23 16:18:47 -0600

    Campo electrico anillo

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,15 +1,23 @@
 from scipy import integrate
 import numpy as np 
 
-epsilo = "{:.2e}".format(0.00000000000885)
+epsilon = 8.85e-12
+
 
 def anillo(distancia, radio, carga):
-    dq = (carga / (2 * np.pi *radio))
 
-    E = (1/(4 * np.pi * epsilo) (dq/()))
+    cos= distancia/pow((distancia**2 + radio**2), 1/2)
 
+    def f(s):
+        return 1
+    integral, error = integrate.quad(f, 0, (2 * np.pi * radio))
+    dq = (carga / (2 * np.pi * radio)) * integral
 
-    return distancia * radio
+    E = (1 / (4 * np.pi * epsilon)) * (dq / (distancia**2 + radio**2)) * cos
+
+    print("El campo electrico es:", E)
+
+    return E
 
 def lineal():
     return
@@ -17,3 +25,4 @@ def lineal():
 def disco():
     return
 
+E = anillo(4, 2, 2)
