089049aca5f23a9a39b5fac05daf6e1656059296
Sebastian <thiagohugo2014@gmail.com>
2023-08-23 17:30:14 -0600

    disco y lineal hechos, faltan detalles

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -19,10 +19,35 @@ def anillo(distancia, radio, carga):
 
     return E
 
-def lineal():
+def lineal(largo, distancia, carga):
+    r= distancia**2 + largo**2
+    cos = distancia/(r**1/2)
+    sen = largo/(r**1/2)
+
+    def f(s):
+        return 1
+    media= largo/2
+    integral, error= integrate.quad(f, -media, media)
+
+    lam= carga/largo
+    r= distancia**2 + largo**2
+
+    Ex= (1 / (4 * np.pi * epsilon)) * (lam/r) * cos * integral
+    print("El campo electrico es:", Ex)
+
+    return Ex
+
+def disco(distancia, radio, carga):
+    def f(r):
+        return r/((distancia**2 + r**2)**3/2)
+    
+    integral, error= integrate.quad(f, 0, radio)
+
+    gama= (carga / (2 * np.pi * (radio**2)))
+
+    E = ((gama * distancia)/(2 * epsilon)) * integral
+
+    print("El campo electrico es:", E)
     return
 
-def disco():
-    return
-
-E = anillo(4, 2, 2)
+E = lineal(4, 3, 2)
