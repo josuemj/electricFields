4610c2b0a770d89d31369d740bcfbf69fbb834d2
Sebastian <thiagohugo2014@gmail.com>
2023-08-23 15:41:30 -0600

    first commit

---
+++ Ejercicio5.py
@@ -0,0 +1,19 @@
+from scipy import integrate
+import numpy as np 
+
+epsilo = "{:.2e}".format(0.00000000000885)
+
+def anillo(distancia, radio, carga):
+    dq = (carga / (2 * np.pi *radio))
+
+    E = (1/(4 * np.pi * epsilo) (dq/()))
+
+
+    return distancia * radio
+
+def lineal():
+    return
+
+def disco():
+    return
+
