e5270b09f989298150136460c08bfe6289f55019
josuemj <98498772+josuemj@users.noreply.github.com>
2023-08-25 20:52:22 -0300

    UI completo

--- uifile.py
+++ uifile.py
@@ -6,10 +6,11 @@ from matplotlib.figure import Figure
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from Ejercicio5 import*
 
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""
-====== FUNCIONES MATEAMATICAS PARA GRAFICAR =========
+====== FUNCIONES MATEAMÁTICAS PARA GRAFICAR =========
 """""""""""""""""""""""""""""""""""""""""""""""""""""
 
 def f(x, R):
@@ -28,7 +29,6 @@ def rectaPositiva(x,radio_intercepto_y,intercepto_x):
     return(radio_intercepto_y/intercepto_x)*x-radio_intercepto_y
 
 
-
 """""""""""""""""""""""""""""""""""""""""""""""""""""
 ============== CREACION DE INTERRFAZ ===============
 """""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -38,6 +38,7 @@ class MyGUI(QMainWindow):
     def __init__(self):
         super(MyGUI,self).__init__()
         uic.loadUi("interface.ui",self)
+
         self.pushButton.clicked.connect(self.campoElectrico)
         self.radioSlider.valueChanged.connect(self.updateRadioLabel)
         self.xSlider.valueChanged.connect(self.updatePosicionLabel)
@@ -80,44 +81,44 @@ class MyGUI(QMainWindow):
         new_layout = QVBoxLayout(self.graphFrame)
         new_layout.addWidget(canvas)
 
+        #Mostrar pantalla
         self.show()
 
     def campoElectrico(self):
 
-        print("Button has been pressed")
+        print("Button has been pressed") #Control
         selected_item_text = self.fieldsComboBox.currentText()
         print("Selected figure: "+selected_item_text)
 
         radio = int(self.radioSlider.value())
-        print("Valor actual del radio"+str(radio))
+        print("Valor actual del radio"+str(radio)) #Control
 
         posicion_x = int(self.xSlider.value())
-        print("posicion en x(m)" + str(posicion_x))
+        print("posicion en x(m)" + str(posicion_x)) #Control
 
 
 
         if selected_item_text == "Anillo":
-            print("Anillo seleccionado")
+            print("Anillo seleccionado") #Control
             self.graficarAnillo(radio,posicion_x)
 
         if selected_item_text == "Disco":
-            print("Disco seleccionado")
-            self.graficarDicos()
+            print("Disco seleccionado") #Control
+            self.graficarDicos(radio,posicion_x)
 
         if selected_item_text == "Linea":
-            print("Linea seleccionada")
-            self.graficarLinea()
+            print("Linea seleccionada") #Control
+            self.graficarLinea(radio,posicion_x)
 
     def graficarAnillo(self,r,posicion_x):
 
         self.clearGraphFrame() #Limpiando el grafico anterior
         puntoP = "(0,"+str(posicion_x)+")"
 
-
         x = np.linspace(-0.5,0.5, 1000) #Dominio de la elipse
         dominioRecta = np.linspace(0,posicion_x,1000) #Dominio de la recta
         fig, ax = plt.subplots()
-        # Customize the plot for "Anillo"
+
         ax.plot(x, f(x, r), color='blue')
         ax.plot(x, h(x, r), color='blue')
 
@@ -127,6 +128,7 @@ class MyGUI(QMainWindow):
 
         plt.scatter(posicion_x, 0, color='red', zorder=2)
         plt.text(posicion_x, 2, puntoP, fontsize=16, color='black')
+
         ax.set_title('Campo eléctrico de anillo')
         ax.grid(True)
 
@@ -134,11 +136,67 @@ class MyGUI(QMainWindow):
         layout = self.graphFrame.layout()
         layout.addWidget(canvas)
 
-    def graficarDicos(self):
-        self.clearGraphFrame()
+        campo_anillo = anillo(posicion_x,r)
+        print("Campo anillo "+str(campo_anillo))
 
-    def graficarLinea(self):
+        self.campoLabel.setText("Campo eléctrico "+str(campo_anillo)+ " N/C")
+
+    def graficarDicos(self,r,posicion_x):
+
         self.clearGraphFrame()
+        puntoP = "(0," + str(posicion_x) + ")"
+
+        x = np.linspace(-0.5, 0.5, 1000)  # Dominio de la elipse
+        dominioRecta = np.linspace(0, posicion_x, 1000)  # Dominio de la recta
+        fig, ax = plt.subplots()
+
+        # Customize the plot for "Anillo"
+        ax.plot(x, f(x, r), color='blue')
+        ax.plot(x, h(x, r), color='blue')
+        ax.fill_between(x, f(x, r), h(x, r), color='blue', alpha=0.3)
+
+        # rectas al punto P
+        ax.plot(dominioRecta, rectaNegativa(dominioRecta, r, posicion_x))
+        ax.plot(dominioRecta, rectaPositiva(dominioRecta, r, posicion_x))
+
+        plt.scatter(posicion_x, 0, color='red', zorder=2)
+        plt.text(posicion_x, 2, puntoP, fontsize=16, color='black')
+        ax.set_title('Campo eléctrico disco')
+        ax.grid(True)
+
+        canvas = FigureCanvas(fig)
+        layout = self.graphFrame.layout()
+        layout.addWidget(canvas)
+
+        campo_disco = disco(posicion_x,r)
+        print("campo electrico Disco: "+str(campo_disco))
+        self.campoLabel.setText("Campo eléctrico " + str(campo_disco) + " N/C")
+
+
+    def graficarLinea(self,r,posicion_x):
+        self.clearGraphFrame()
+        puntoP = "(0," + str(posicion_x) + ")"
+        dominioRecta = np.linspace(0, posicion_x, 1000)  # Dominio de la recta
+
+
+        fig, ax = plt.subplots()
+        ax.plot([0, 0], [r, -r], color='b', linestyle='-', marker='o')
+        ax.plot(dominioRecta, rectaNegativa(dominioRecta, r, posicion_x))
+        ax.plot(dominioRecta, rectaPositiva(dominioRecta, r, posicion_x))
+        ax.text(posicion_x-0.2, 1.2, puntoP, fontsize=16, color='black')
+
+        ax.scatter([0, 0], [r, -r], color='r', label='Puntos')
+        ax.grid(True)
+        ax.scatter(posicion_x, 0, color='red', zorder=2)
+
+        ax.set_title('Campo eléctro linea')
+        canvas = FigureCanvas(fig)
+        layout = self.graphFrame.layout()
+        layout.addWidget(canvas)
+
+        campoLinea = lineal(r*2,posicion_x) #Valor campo electrico
+        self.campoLabel.setText("Campo eléctrico "+str(campoLinea)+" N/C")
+
 
 
     def clearGraphFrame(self):
